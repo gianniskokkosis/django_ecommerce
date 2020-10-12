72df3ec98d53d981de378478e045fcecbcd0b328
gianniskokkosis <t8160190@aueb.gr>
2020-10-12 12:59:38 +0300

    Initial commit

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.status
+
+class Invoice(models.Model):
+    name = models.CharField(max_length=200)
+    surname = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
