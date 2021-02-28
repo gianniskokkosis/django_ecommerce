f176a945d3118b9093fdc18a7a1afd6e7e4d3659
gianniskokkosis <t8160190@aueb.gr>
2021-02-28 11:16:39 +0200

    Add Wishlist to admin models

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Invoice, Product
+from .models import Invoice, Product, Wishlist
 
 admin.site.register(Product)
 admin.site.register(Invoice)
+admin.site.register(Wishlist)
