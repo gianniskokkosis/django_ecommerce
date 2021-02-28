eab5728685765ef37521081904ee0db965e6edd1
gianniskokkosis <t8160190@aueb.gr>
2021-02-28 10:37:15 +0200

    Add Wishlist Model

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -10,6 +10,12 @@ class Product(models.Model):
         return self.name
 
 
+class Wishlist(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.name
+
 
 class Invoice(models.Model):
     name = models.CharField(max_length=200)
